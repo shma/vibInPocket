5034de537d5e5f1989edac7d9e797a183b698d8c
Shunya <id.shma@gmail.com>
2017-06-08 18:46:03 +0900

    Initial Commit

---
+++ vibInPocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vibInPocket
+//
+//  Created by Matsuno Shunya on 2017/06/08.
+//  Copyright © 2017年 Matsuno Shunya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
